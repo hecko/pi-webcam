175105a29ee19a05a5f87bee27cb66ce03a7c13d
root <root@raspberrypi.(none)>
2013-07-21 10:39:07 +0000

    debugging serial

---
+++ bin/serial.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use FindBin qw<$Bin>;
+use lib "$Bin/../lib";
+
+use common::sense;
+use Utils;
+
+my $serial = get_serial();
+
+say "$serial";
--- lib/Utils.pm
+++ lib/Utils.pm
@@ -12,6 +12,9 @@ sub get_serial {
     my $mac = set_input(`cat /sys/class/net/eth0/address`);
     my $cpu = set_input(`cat /proc/cpuinfo | grep Serial | awk '{ print \$3}'`);
 
+    say "MAC:        $mac";
+    say "CPU serial: $cpu";
+
     my $crypt = crypt($cpu,$mac);
     $crypt =~ s/(\.)|(\/)//g;
     $crypt = lc($crypt);
